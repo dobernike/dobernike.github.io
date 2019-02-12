e298419f1827e8388935258983c80a039de89356
dobernike <dobernike@mail.ru>
2019-02-12 21:02:50 +0300

    module4 task-15

--- javascript/Кекстаграм/js/pictures.js
+++ javascript/Кекстаграм/js/pictures.js
@@ -59,31 +59,32 @@ var addFragment = function (arr) {
 
 addFragment(photos);
 
-
-// var renderBigPhoto = function (photo) {
-//   var photoElement = socialComment.cloneNode(true);
-
-//   photoElement.querySelector('img').src = 'img/avatar-' + randomInteger(1, 6) + '.svg';
-//   photoElement.querySelector('.social__text').textContent = photo.comments[0];
-
-// console.log(photo.comments[0]);
-// var comment = '';
-// for (var i = 0; i < photo.comments.length; i++) {
-//   comment += '<li class="social__comment social__comment--text"><img class="social__picture" src="img/avatar-' + randomInteger(1, 6) + '.svg" alt="Аватар комментатора фотографии" width="35" height="35"> <p class="social__text">' + photo.comments[i] + '</p></li>';
-// }
-// console.log(comment);
-
-// var comments = bigPhoto.querySelector('social__comments');
-// comments.innerHTML(comment);
-
-//   return photoElement;
-// };
-
 var bigPhoto = document.querySelector('.big-picture');
+
+var onBigPhotoPressESC = function (evt) {
+  if (evt.keyCode === ESCAPE_KEYCODE) {
+    bigPhoto.classList.add('hidden');
+    closeBigPhoto();
+  }
+};
+
+var closeBigPhoto = function () {
+  bigPhoto.classList.add('hidden');
+  document.removeEventListener('keydown', onBigPhotoPressESC);
+};
+
+
+var closeButtonBigPhoto = bigPhoto.querySelector('.big-picture__cancel');
+closeButtonBigPhoto.addEventListener('click', function () {
+  closeBigPhoto();
+});
+
 
 var doBigPhoto = function (photo) {
 
   bigPhoto.classList.remove('hidden');
+  document.addEventListener('keydown', onBigPhotoPressESC);
+
 
   bigPhoto.querySelector('img').src = photo.url;
   bigPhoto.querySelector('.likes-count').textContent = photo.likes;
@@ -101,11 +102,163 @@ var doBigPhoto = function (photo) {
   return bigPhoto;
 };
 
-doBigPhoto(photos[0]);
 
-var hideIt = function (needToHide) {
+// doBigPhoto(photos[3]);
+
+var addPictureLinkHandler = function (picture, photo) {
+  picture.addEventListener('click', function (evt) {
+    evt.preventDefault();
+    doBigPhoto(photo);
+  });
+};
+
+var picturesLinks = document.querySelectorAll('.picture');
+// console.log(picturesLinks);
+
+for (var j = 0; j < picturesLinks.length; j++) {
+  var pictureItem = picturesLinks[j];
+  addPictureLinkHandler(pictureItem, photos[j]);
+}
+
+// var closeBigPhoto =
+
+
+var hide = function (needToHide) {
   needToHide.classList.add('visually-hidden');
 };
 
-hideIt(document.querySelector('.social__comment-count'));
-hideIt(document.querySelector('.comments-loader'));
+hide(document.querySelector('.social__comment-count'));
+hide(document.querySelector('.comments-loader'));
+
+
+// preview-effect
+var ESCAPE_KEYCODE = 27;
+var SCALE_PIN_MAX = 100;
+var SCALE_PIN_MIN = 0;
+var EFFECT_CHROME_MIN = 0;
+var EFFECT_CHROME_MAX = 1;
+var EFFECT_SEPIA_MIN = 0;
+var EFFECT_SEPIA_MAX = 1;
+var EFFECT_MARVIN_MIN = 0;
+var EFFECT_MARVIN_MAX = 100;
+var EFFECT_FOBOS_MIN = 0;
+var EFFECT_FOBOS_MAX = 5;
+var EFFECT_HEAT_MIN = 0;
+var EFFECT_HEAT_MAX = 3;
+
+var uploadFile = document.querySelector('#upload-file');
+var imgUploadOverlay = document.querySelector('.img-upload__overlay');
+var uploadCancel = imgUploadOverlay.querySelector('#upload-cancel');
+var effectLevelLine = imgUploadOverlay.querySelector('.effect-level__line');
+var effectLevelDepth = imgUploadOverlay.querySelector('.effect-level__depth');
+var filterChecked = imgUploadOverlay.querySelector('.effects__radio:checked + .effects__label > .effects__preview');
+var effectsPreviews = imgUploadOverlay.querySelectorAll('.effects__preview');
+var scaleControlValue = imgUploadOverlay.querySelector('.scale__control--value');
+var imagePreview = imgUploadOverlay.querySelector('.img-upload__preview > img');
+var scalePin = imgUploadOverlay.querySelector('.effect-level__pin');
+var scalePinProcent = scalePin.style.left;
+
+var pinEditor = function () {
+  // imagePreview.style.filter = getComputedStyle(filterChecked).filter;
+
+  // var EFFECT_SEPIA_MIN = 0;
+  // var EFFECT_SEPIA_MAX = 1;
+  // var SCALE_PIN_MAX = 100;
+  // var filterValueTest = 'sepia(' + 1 + ')';
+  // scalePinProcent = 100;
+  // var math = ((SCALE_PIN_MIN + scalePinProcent) * 1);
+  // console.log(math);
+  // console.log(scalePinProcent);
+  // var filterValueTest = 'invert(' + (SCALE_PIN_MIN + scalePinProcent) * 1 + '%)';
+  // console.log(filterValueTest);
+  // console.log(scalePinProcent);
+
+
+  var filterValue = '';
+  switch (imagePreview.className) {
+    case 'effects__preview--none':
+      filterValue = 0;
+      break;
+    case 'effects__preview--chrome':
+      filterValue = 'grayscale(' + ((SCALE_PIN_MIN + scalePinProcent) * 0.01) + ')';
+      break;
+    case 'effects__preview--sepia':
+      filterValue = 'sepia(' + ((SCALE_PIN_MIN + scalePinProcent) * 0.01) + ')';
+      break;
+    case 'effects__preview--marvin':
+      filterValue = 'invert(' + (SCALE_PIN_MIN + scalePinProcent) * 1 + '%)';
+      break;
+    case 'effects__preview--phobos':
+      filterValue = 'blur(' + ((SCALE_PIN_MIN + scalePinProcent) / 20) + 'px)';
+      break;
+    case 'effects__preview--heat':
+      filterValue = 'brightness(' + ((SCALE_PIN_MIN + scalePinProcent) / 20) + ')';
+      break;
+    default:
+      break;
+  }
+
+
+  imagePreview.style.filter = filterValue;
+  // SCALE_PIN_MAX = 0;
+  // resetEditor();
+};
+
+
+scalePin.addEventListener('mouseup', function () {
+  pinEditor();
+});
+
+
+var resetEditor = function () {
+  effectLevelDepth.style.width = SCALE_PIN_MAX;
+  scalePin.style.left = SCALE_PIN_MAX;
+  scaleControlValue.value = SCALE_PIN_MAX + '%';
+  imagePreview.className = '';
+}; resetEditor();
+
+// Делегирование еффектов
+var addEventToEffect = function (effect) {
+  effect.addEventListener('click', function () {
+    imagePreview.className = effect.classList[1];
+  });
+};
+
+var effectPreviews = imgUploadOverlay.querySelectorAll('.effects__preview');
+for (var i = 0; i < effectPreviews.length; i++) {
+  var effect = effectPreviews[i];
+  addEventToEffect(effect);
+}
+
+
+// ------------------------------------------------------------
+
+
+// Открывание и закрывание редактора
+var onEditorPressESC = function (evt) {
+  if (evt.keyCode === ESCAPE_KEYCODE) {
+    closeEditor();
+  }
+};
+
+var openEditor = function () {
+  imgUploadOverlay.classList.remove('hidden');
+
+  document.addEventListener('keydown', onEditorPressESC);
+  pinEditor();
+};
+
+var closeEditor = function () {
+  imgUploadOverlay.classList.add('hidden');
+  uploadFile.value = '';
+
+  document.removeEventListener('keydown', onEditorPressESC);
+};
+
+uploadFile.addEventListener('change', function () {
+  openEditor();
+});
+
+uploadCancel.addEventListener('click', function () {
+  closeEditor();
+});
