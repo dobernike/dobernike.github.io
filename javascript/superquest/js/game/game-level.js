56d7ea296e38e0d632f9edb7259c8de783065ae5
dobernike <dobernike@mail.ru>
2019-02-28 12:18:36 +0300

    start new

--- javascript/superquest/js/game/game-level.js
+++
@@ -1,9 +0,0 @@
-export default (level) => {
-  return `<div class="quest">
-  <p class="text">${level.text}</p>
-  <input type="text">
-  <ul class="answers">
-    ${level.answers.map((it) => `<li class="answer">${it}</li>`).join(``)}
-  </ul>  
-</div>`;
-};
