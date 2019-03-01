763b676890a6d6b88b7bccf4337873f0474813bb
dobernike <Dobernike@mail.ru>
2019-03-01 21:49:12 +0300

    Начало построенние логики в модулях

--- javascript/pixel-hunter/js/intro.js
+++ javascript/pixel-hunter/js/intro.js
@@ -1,8 +1,19 @@
-import { render } from './util.js';
+import { render, changeScreen } from './util.js';
+import greeting from './greeting.js';
 
 const template = `<section class="intro">
 <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
 <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
 </section>`;
+
+const introAsterisk = document.querySelector(`.intro__asterisk`);
+// console.log(introAsterisk);
+
+// introAsterisk.onclick = () => {
+//   changeScreen(greeting);
+// };
+// introAsterisk.addEventListener(`click`, () => {
+//   changeScreen(greeting);
+// });
 
 export default render(template);
