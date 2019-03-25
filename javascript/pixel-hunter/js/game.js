53a259143a2fa9c82c20e21ffc032add5c21a3b1
dobernike <Dobernike@mail.ru>
2019-03-25 15:08:21 +0300

    Описание структуры данных игры

--- javascript/pixel-hunter/js/game.js
+++ javascript/pixel-hunter/js/game.js
@@ -1,63 +1,17 @@
-const games = {
-  'double': {
-    question: {
-      text: `Угадайте для каждого изображения фото или рисунок?`,
-      option: `<div class="game__option">
-      <img src="${games.answers.first.src}" alt="Option 1" width="468" height="458">
-      <label class="game__answer game__answer--photo">
-        <input class="visually-hidden" name="question1" type="radio" value="photo">
-        <span>Фото</span>
-      </label>
-      <label class="game__answer game__answer--paint">
-        <input class="visually-hidden" name="question1" type="radio" value="paint">
-        <span>Рисунок</span>
-      </label>
-    </div>`,
-      answers: {
-        first: {
-          itIsPhoto: true,
-          src: `img[0]`
-        },
-        second: {
-          itIsPhoto: false,
-          src: `http://`
-        }
+export const games = {
+  type: `double`,
+  question: {
+    text: `Угадайте для каждого изображения фото или рисунок?`,
+    answers: {
+      first: {
+        itIsPhoto: true,
+        src: `url(...)`
+      },
+      second: {
+        itIsPhoto: false,
+        src: `url(...)`
       }
-    },
-    statistic: null
+    }
   },
-  'wide': {
-    question: {
-      text: ``,
-      isPhoto: false,
-      answers: {
-        first: {
-          itIsPhoto: true,
-          src: `http://`
-        }
-      }
-    },
-    statistic: null
-  },
-  'triple': {
-    question: {
-      text: ``,
-      isPhoto: false,
-      answers: {
-        first: {
-          itIsPhoto: true,
-          src: `http://`
-        },
-        second: {
-          itIsPhoto: false,
-          src: `http://`
-        },
-        third: {
-          itIsPhoto: false,
-          src: `http://`
-        }
-      }
-    },
-    statistic: null
-  }
+  statistic: null
 };
