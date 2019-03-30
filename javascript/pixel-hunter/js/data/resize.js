2bc88745c0915aecf083218510ea2053f9887063
dobernike <Dobernike@mail.ru>
2019-03-30 21:58:46 +0300

    Исправляет размеры

--- javascript/pixel-hunter/js/data/resize.js
+++ javascript/pixel-hunter/js/data/resize.js
@@ -8,54 +8,54 @@ export const resize = (expected, given) => {
     if (given.width === given.height) {
       height = 256;
       width = 256;
-    } else if (given.width > given.height) {
+    } else if (given.width < given.height) {
       height = 256;
       width = 128;
-    } else if (given.width < given.height) {
+    } else if (given.width > given.height) {
       height = 128;
       width = 256;
     }
   } else if (expected.height === 256 && expected.width === 128) {
     if (given.width === given.height) {
-      height = 256;
       width = 128;
-    } else if (given.width > given.height) {
+      height = 256;
+    } else if (given.width < given.height) {
       height = 256;
       width = 64;
-    } else if (given.width < given.height) {
+    } else if (given.width > given.height) {
       height = 128;
       width = 128;
     }
   } else if (expected.height === 468 && expected.width === 458) {
     if (given.width === given.height) {
-      height = 468;
-      width = 458;
-    } else if (given.width > given.height) {
+      height = 458;
+      width = 468;
+    } else if (given.width < given.height) {
       height = 468;
       width = 229;
-    } else if (given.width < given.height) {
+    } else if (given.width > given.height) {
       height = 234;
       width = 458;
     }
   } else if (expected.height === 705 && expected.width === 455) {
     if (given.width === given.height) {
-      height = 705;
-      width = 455;
-    } else if (given.width > given.height) {
+      height = 455;
+      width = 705;
+    } else if (given.width < given.height) {
       height = 705;
       width = 227;
-    } else if (given.width < given.height) {
+    } else if (given.width > given.height) {
       height = 352;
       width = 455;
     }
   } else if (expected.height === 304 && expected.width === 455) {
     if (given.width === given.height) {
-      height = 304;
-      width = 455;
-    } else if (given.width > given.height) {
+      height = 455;
+      width = 304;
+    } else if (given.width < given.height) {
       height = 304;
       width = 227;
-    } else if (given.width < given.height) {
+    } else if (given.width > given.height) {
       height = 152;
       width = 455;
     }
@@ -66,5 +66,3 @@ export const resize = (expected, given) => {
     width
   };
 };
-
-
