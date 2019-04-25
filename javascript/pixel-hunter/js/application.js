4bf289c9d0c1d402f48506e71e0c6d4e606c7c75
dobernike <dobernike@mail.ru>
2019-04-25 22:54:25 +0300

    Создает класс Application, с ролью роутера(Router)

---
+++ javascript/pixel-hunter/js/application.js
@@ -0,0 +1,28 @@
+/* eslint-disable object-curly-spacing */
+import IntroScreen from './screens/intro-screen.js';
+import { changeScreen } from './utils/util.js';
+import GameModel from './model/game-model.js';
+import GameScreen from './screens/game-screen.js';
+import StatsScreen from './screens/stats-screen.js';
+
+
+export default class Application {
+
+  static showWelcome() {
+    const welcome = new IntroScreen();
+    changeScreen(welcome.element);
+  }
+
+  static showGame(userName) {
+    const model = new GameModel(userName);
+    const gameScreen = new GameScreen(model);
+    changeScreen(gameScreen.element);
+    gameScreen.startGame();
+  }
+
+  static showStats(stats) {
+    const statistics = new StatsScreen(stats);
+    changeScreen(statistics.element);
+  }
+  
+}
