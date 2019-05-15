e67471141e17761d272bd377e26d3df0bc6fd303
dobernike <Dobernike@mail.ru>
2019-05-15 15:10:38 +0300

    Добавляет star-db в проект

---
+++ javascript/star-db/precache-manifest.80aaeb159011529f585280c09ba7e289.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdf4ffe40dc35740d1956d6d05e4df3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a535fbf56261625913c",
+    "url": "/static/css/main.a6e2a475.chunk.css"
+  },
+  {
+    "revision": "f8f1d4252d951e0b4f37",
+    "url": "/static/js/2.62ccfc19.chunk.js"
+  },
+  {
+    "revision": "2a535fbf56261625913c",
+    "url": "/static/js/main.79a3ba79.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
