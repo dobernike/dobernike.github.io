53e9ddeb3765ef91349c7041ce46cf570280bbac
dobernike <Dobernike@mail.ru>
2019-05-25 18:02:16 +0300

    Добавляет новый проект в портфолио

---
+++ javascript/re-store/precache-manifest.d66c135eb6c160bcd3939c1d0567df9e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b5ae37e0a7f4fefe72888bb929b4fc5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "98e06597c08470eac952",
+    "url": "/static/css/main.dcafc435.chunk.css"
+  },
+  {
+    "revision": "fa53c49f357ac289899d",
+    "url": "/static/js/2.6cc75215.chunk.js"
+  },
+  {
+    "revision": "98e06597c08470eac952",
+    "url": "/static/js/main.159fb335.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
