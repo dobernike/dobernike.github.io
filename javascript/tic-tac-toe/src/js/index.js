bc1c335e6f1452049df506e5f1437bebee5cf821
dobernike <dobernike@mail.ru>
2019-07-15 23:05:39 +0300

    Добавляет background

--- javascript/tic-tac-toe/src/js/index.js
+++ javascript/tic-tac-toe/src/js/index.js
@@ -6,26 +6,56 @@ const gameTable = game.querySelector('.table');
 
 const tableCell = '<div class="table__cell"></div>';
 
-const playerO = 'O';
-const playerX = 'X';
-let currentPlayer = playerO;
+const viewScoreO = game.querySelector('.playerO-score');
+const viewScoreX = game.querySelector('.playerX-score');
+
+const PLAYERS = {
+  'O': {
+    name: 'O',
+    score: 0,
+    positions: []
+  },
+  'X': {
+    name: 'X',
+    score: 0,
+    positions: []
+  }
+}
+
+let currentPlayer = PLAYERS['O'];
 
 const turn = game.querySelector('.player-turn');
 
 function updateGame(cell) {
   cell.removeEventListener('click', cellClickHandler)
 
-  currentPlayer === playerO ? currentPlayer = playerX : currentPlayer = playerO;
+  currentPlayer === PLAYERS['O'] ? currentPlayer = PLAYERS['X'] : currentPlayer = PLAYERS['O'];
 
-  turn.innerText = currentPlayer;
+  turn.innerText = currentPlayer.name;
+}
+
+function canContinue() {
+  return true;
+}
+
+function startNextGame(winner) {
+  winner = winner.score + 1;
+  createGameTable(gameSize.value);
 }
 
 function cellClickHandler() {
-  this.innerText = currentPlayer;
-  updateGame(this);
+  this.innerText = currentPlayer.name;
+  if (canContinue()) {
+    updateGame(this);
+  } else {
+    startNextGame(currentPlayer)
+  }
 }
 
 function createGameTable(size) {
+  viewScoreO.innerHTML = PLAYERS['O'].score;
+  viewScoreX.innerHTML = PLAYERS['X'].score;
+
   gameTable.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
   gameTable.innerHTML = tableCell.repeat(size ** 2);
 
@@ -42,11 +72,23 @@ form.addEventListener('submit', e => {
   game.classList.remove('hidden');
 });
 
-
-
 const repeat = game.querySelector('.repeat');
 repeat.addEventListener('click', () => {
-  currentPlayer = playerO;
+
+  PLAYERS['O'] = {
+    name: 'O',
+    score: 0,
+    positions: []
+  }
+
+  PLAYERS['X'] = {
+    name: 'X',
+    score: 0,
+    positions: []
+  }
+
+  currentPlayer = PLAYERS['O'];
+
   game.classList.add('hidden');
   setting.classList.remove('hidden');
 });
