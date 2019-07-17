a210e12abaef5ea4089525809bd9e4201a738074
dobernike <dobernike@mail.ru>
2019-07-17 14:58:43 +0300

    Установка кеша

--- javascript/tic-tac-toe/src/service-worker.js
+++ javascript/tic-tac-toe/src/service-worker.js
@@ -1,11 +1,28 @@
+const CACHE = 'cache-only-v1';
+const PRECACHE_URLS = [
+  'index.html',
+  './', // Alias for index.html
+  './css/index.css',
+  './js/index.js'
+];
+
+// При установке воркера мы должны закешировать часть данных (статику).
 self.addEventListener('install', (event) => {
-  console.log('Установлен');
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => {
+      return cache.addAll([PRECACHE_URLS]);
+    })
+  );
 });
 
-self.addEventListener('activate', (event) => {
-  console.log('Активирован');
-});
+// При запросе на сервер (событие fetch), используем только данные из кэша.
+self.addEventListener('fetch', (event) =>
+  event.respondWith(fromCache(event.request))
+);
 
-self.addEventListener('fetch', (event) => {
-  console.log('Происходит запрос на сервер');
-});
+function fromCache(request) {
+  return caches.open(CACHE).then((cache) =>
+    cache.match(request)
+      .then((matching) => matching || Promise.reject('no-match'))
+  );
+}
