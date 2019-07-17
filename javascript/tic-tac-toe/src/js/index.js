25b0ffc0018bda2ba91d58af67ad50fa9c0d37b8
dobernike <dobernike@mail.ru>
2019-07-17 14:35:51 +0300

    Добавляет инициализацию service-worker

--- javascript/tic-tac-toe/src/js/index.js
+++ javascript/tic-tac-toe/src/js/index.js
@@ -1,19 +1,5 @@
-// register service worker
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/javascript/tic-tac-toe/src/js/sw.js', { scope: '/javascript/tic-tac-toe/src/js/' }).then(function (reg) {
-
-    if (reg.installing) {
-      console.log('Service worker installing');
-    } else if (reg.waiting) {
-      console.log('Service worker installed');
-    } else if (reg.active) {
-      console.log('Service worker active');
-    }
-
-  }).catch(function (error) {
-    // registration failed
-    console.log('Registration failed with ' + error);
-  });
+  navigator.serviceWorker.register('service-worker.js');
 }
 
 const form = document.querySelector('.setting__form');
