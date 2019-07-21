088ce15779b3cad269d2c54050ddb6243ab9ece4
dobernike <dobernike@mail.ru>
2019-07-21 16:30:51 +0300

    Добавляет invoice project

---
+++ invoice/precache-manifest.41ed827ab492b22562d24d57d4250b3a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a387fb9943475d830acc3c6e76f72569",
+    "url": "/invoice/index.html"
+  },
+  {
+    "revision": "9b7c995a99c1ef6dc840",
+    "url": "/invoice/static/css/main.f0608e4a.chunk.css"
+  },
+  {
+    "revision": "5c9ff16ec5bee154aee2",
+    "url": "/invoice/static/js/2.5286435d.chunk.js"
+  },
+  {
+    "revision": "9b7c995a99c1ef6dc840",
+    "url": "/invoice/static/js/main.eee469cc.chunk.js"
+  },
+  {
+    "revision": "e6f7ca31bf4f5f358fb5",
+    "url": "/invoice/static/js/runtime~main.8c53ff36.js"
+  }
+]);
