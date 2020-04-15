0f5e027a5bee2877fad8bed10c17e737081ed731
dobernike <Dobernike@mail.ru>
2020-04-15 10:39:53 +0300

    Added project

---
+++ javascript/monsters-rolodex/precache-manifest.230f695f6fa1bea4ef9f81c25a60d39f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65c61688584aefbb7ea390c2629a45b7",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "320fd1bfd29ad10987ce",
+    "url": "/monsters-rolodex/static/css/main.4b66cd88.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "320fd1bfd29ad10987ce",
+    "url": "/monsters-rolodex/static/js/main.4055d3b7.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
